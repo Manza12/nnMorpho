2d2fd02adcf3b54a11ae25d0ae84f516e05aa1a3
Manza12 <tritery@hotmail.com>
2021-11-04 19:50:46 +0100

    New version (2.0.2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name='nnMorpho',
   packages=['nnMorpho'],
-  version='2.0.1',
+  version='2.0.2',
   license='MIT',
   description='A library for GPU-accelerated and Machine-Learning adapted'
               ' Mathematical Morphology',
