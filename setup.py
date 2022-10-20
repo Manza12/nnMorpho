d2cb5880a981513457a8237c75f68c43889369ce
Gon <tritery@hotmail.com>
2022-10-20 20:34:31 +0200

    Creating a clean setup.py to install all at once properly

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
 from setuptools import setup
 from torch.utils import cpp_extension
 
-setup(name='morphology_cpp',
-      version='0.0.1',
-      ext_modules=[
-          cpp_extension.CppExtension('binary_morphology_cpp', ['C++/binary_morphology.cpp']),
-          cpp_extension.CppExtension('cylindrical_binary_morphology_cpp', ['C++/cylindrical_binary_morphology.cpp']),
-          cpp_extension.CppExtension('greyscale_morphology_cpp', ['C++/greyscale_morphology.cpp'])
-      ],
-      cmdclass={'build_ext': cpp_extension.BuildExtension}
-      )
-
-setup(
-    name='morphology_cuda',
-    version='0.0.1',
-    where='./CUDA',
-    ext_modules=[
-        cpp_extension.CUDAExtension('morphology_cuda', [
-            'CUDA/morphology.cpp',
-            'CUDA/morphology_cuda.cu',
-        ])
-    ],
-    cmdclass={
-        'build_ext': cpp_extension.BuildExtension
-    })
-
 setup(
     name='nnMorpho',
     packages=['nnMorpho'],
@@ -51,4 +27,14 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    ext_modules=[
+        cpp_extension.CppExtension('binary_morphology_cpp', ['C++/binary_morphology.cpp']),
+        cpp_extension.CppExtension('cylindrical_binary_morphology_cpp', ['C++/cylindrical_binary_morphology.cpp']),
+        cpp_extension.CppExtension('greyscale_morphology_cpp', ['C++/greyscale_morphology.cpp']),
+        cpp_extension.CUDAExtension('morphology_cuda', [
+            'CUDA/morphology.cpp',
+            'CUDA/morphology_cuda.cu',
+        ])
+      ],
+    cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
