f160e693612d931aaffa6f0f730edfefa7a47cd2
Manza12 <tritery@hotmail.com>
2023-03-29 13:18:48 +0200

    Remove CUDA folder

--- C++/greyscale_operators.cu
+++ C++/greyscale_operators.cu
@@ -1,6 +1,6 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include "../binary_operators.cpp"
+#include "greyscale_operators.cpp"
 
 /* Kernels */
 // Erosion
@@ -8,10 +8,13 @@ template <typename scalar>
 __global__ void erosion_cuda_kernel(
         const torch::PackedTensorAccessor32<scalar, 2, torch::RestrictPtrTraits> input_accessor,
         const torch::PackedTensorAccessor32<scalar, 2, torch::RestrictPtrTraits> str_el_accessor,
-        torch::PackedTensorAccessor32<bool, 2, torch::RestrictPtrTraits> output_accessor,
+        const torch::PackedTensorAccessor32<bool, 2, torch::RestrictPtrTraits> footprint_accessor,
+        torch::PackedTensorAccessor32<scalar, 2, torch::RestrictPtrTraits> output_accessor,
         const int origin_x,
         const int origin_y,
-        const char border_type) {
+        char border_type,
+        scalar top,
+        scalar bottom) {
 
     // Sizes
     const auto m = input_accessor.size(1);
@@ -24,23 +27,22 @@ __global__ void erosion_cuda_kernel(
     unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     // Declare variables
-    bool value;
+    scalar value = top;
+    scalar difference;
     int idx_x, idx_y;
 
     // Compute the value of output[y][x]
     if (x < m && y < n) {
         for (int j = 0; j < q; j++) {
             for (int i = 0; i < p; i++) {
-                idx_x = x + (i - origin_x);
-                idx_y = y + (j - origin_y);
-                if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
-                    if (str_el_accessor[j][i] > input_accessor[idx_y][idx_x]) {
-                        value = false;
-                        goto end;
-                    }
-                } else if (border_type == 'e') {
-                    if (str_el_accessor[j][i]) {
-                        value = false;
+                if (footprint_accessor[j][i]) {
+                    idx_x = x + (i - origin_x);
+                    idx_y = y + (j - origin_y);
+                    if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
+                        difference = input_accessor[idx_y][idx_x] - str_el_accessor[j][i];
+                        if (value > difference) value = difference;
+                    } else if (border_type == 'e') {
+                        value = bottom;
                         goto end;
                     }
                 }
@@ -53,8 +55,9 @@ __global__ void erosion_cuda_kernel(
 // Dilation
 template <typename scalar>
 __global__ void dilation_cuda_kernel(
-        const torch::PackedTensorAccessor32<bool, 2, torch::RestrictPtrTraits> input_accessor,
+        const torch::PackedTensorAccessor32<scalar, 2, torch::RestrictPtrTraits> input_accessor,
         const torch::PackedTensorAccessor32<scalar, 2, torch::RestrictPtrTraits> str_el_accessor,
+        const torch::PackedTensorAccessor32<bool, 2, torch::RestrictPtrTraits> footprint_accessor,
         torch::PackedTensorAccessor32<scalar, 2, torch::RestrictPtrTraits> output_accessor,
         const int origin_x,
         const int origin_y,
@@ -72,19 +75,19 @@ __global__ void dilation_cuda_kernel(
 
     // Declare variables
     scalar value = bottom;
-    scalar current;
+    scalar sum;
     int idx_x, idx_y;
 
     // Compute the value of output[y][x]
     if (x < m && y < n) {
         for (int j = q-1; j >= 0; j--) {
             for (int i = p-1; i >= 0; i--) {
-                idx_x = x - (i - origin_x);
-                idx_y = y - (j - origin_y);
-                if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
-                    if (input_accessor[idx_y][idx_x]) {
-                        current = str_el_accessor[j][i];
-                        if (value < current) value = current;
+                if (footprint_accessor[j][i]) {
+                    idx_x = x - (i - origin_x);
+                    idx_y = y - (j - origin_y);
+                    if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
+                        sum = input_accessor[idx_y][idx_x] + str_el_accessor[j][i];
+                        if (value < sum) value = sum;
                     }
                 }
             }
@@ -98,11 +101,14 @@ __global__ void dilation_cuda_kernel(
 template <typename scalar>
 torch::Tensor erosion(torch::Tensor input,
                       torch::Tensor str_el,
-                      int origin_y,
+                      torch::Tensor footprint,
                       int origin_x,
+                      int origin_y,
                       char border_type,
-                      const int block_size_y,
-                      const int block_size_x) {
+                      scalar top,
+                      scalar bottom,
+                      const int block_size_x,
+                      const int block_size_y) {
 
     // Compute output size
     const auto m = input.size(1);
@@ -111,7 +117,7 @@ torch::Tensor erosion(torch::Tensor input,
     const auto q = str_el.size(0);
 
     // Initialization
-    auto options = torch::TensorOptions().device(input.device()).dtype(torch::ScalarType::Bool);
+    auto options = torch::TensorOptions().device(input.device()).dtype(input.dtype());
     torch::Tensor output_tensor = torch::zeros({n, m}, options);
 
     // Switch between CPU and GPU
@@ -120,7 +126,8 @@ torch::Tensor erosion(torch::Tensor input,
         // Create accessors
         auto input_accessor = input.packed_accessor32<scalar, 2, torch::RestrictPtrTraits>();
         auto str_el_accessor = str_el.packed_accessor32<scalar, 2, torch::RestrictPtrTraits>();
-        auto output_accessor = output_tensor.packed_accessor32<bool, 2, torch::RestrictPtrTraits>();
+        auto footprint_accessor = footprint.packed_accessor32<bool, 2, torch::RestrictPtrTraits>();
+        auto output_accessor = output_tensor.packed_accessor32<scalar, 2, torch::RestrictPtrTraits>();
 
         // Block & Grid parameters
         const int grid_x = ((m - 1) / block_size_x) + 1;
@@ -130,39 +137,34 @@ torch::Tensor erosion(torch::Tensor input,
         const dim3 grid_size(grid_x, grid_y, 1);
 
         // Launch of the kernel
-        erosion_cuda_kernel<<<grid_size, block_size>>>(
-                input_accessor,
-                str_el_accessor,
-                output_accessor,
-                origin_x, origin_y, border_type);
+        erosion_cuda_kernel<<<grid_size, block_size>>>(input_accessor, str_el_accessor, footprint_accessor,
+                                                       output_accessor, origin_x, origin_y, border_type, top, bottom);
     } else {
         /* CPU */
         // Create accessors
         auto input_accessor = input.accessor<scalar, 2>();
         auto str_el_accessor = str_el.accessor<scalar, 2>();
-        auto output_accessor = output_tensor.accessor<bool, 2>();
+        auto footprint_accessor = footprint.accessor<bool, 2>();
+        auto output_accessor = output_tensor.accessor<scalar, 2>();
 
-        // Declare variables
-        bool value;
+        scalar value;
+        scalar difference;
         int idx_x, idx_y;
-
         // Computation
         for (int y = 0; y < n; y++) {
             for (int x = 0; x < m; x++) {
-                value = true;
+                value = top;
                 // Compute the value of output[y][x]
                 for (int j = 0; j < q; j++) {
                     for (int i = 0; i < p; i++) {
-                        idx_x = x + (i - origin_x);
-                        idx_y = y + (j - origin_y);
-                        if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
-                            if (str_el_accessor[j][i] > input_accessor[idx_y][idx_x]) {
-                                value = false;
-                                goto end;
-                            }
-                        } else if (border_type == 'e') {
-                            if (str_el_accessor[j][i]) {
-                                value = false;
+                        if (footprint_accessor[j][i]) {
+                            idx_x = x + (i - origin_x);
+                            idx_y = y + (j - origin_y);
+                            if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
+                                difference = input_accessor[idx_y][idx_x] - str_el_accessor[j][i];
+                                if (value > difference) value = difference;
+                            } else if (border_type == 'e') {
+                                value = bottom;
                                 goto end;
                             }
                         }
@@ -180,11 +182,12 @@ torch::Tensor erosion(torch::Tensor input,
 template <typename scalar>
 torch::Tensor dilation(torch::Tensor input,
                        torch::Tensor str_el,
-                       int origin_y,
+                       torch::Tensor footprint,
                        int origin_x,
+                       int origin_y,
                        scalar bottom,
-                       int block_size_y,
-                       int block_size_x) {
+                       int block_size_x,
+                       int block_size_y) {
 
     // Compute output size
     const auto m = input.size(1);
@@ -193,15 +196,16 @@ torch::Tensor dilation(torch::Tensor input,
     const auto q = str_el.size(0);
 
     // Initialization
-    auto options = torch::TensorOptions().device(input.device()).dtype(str_el.dtype());
+    auto options = torch::TensorOptions().device(input.device()).dtype(input.dtype());
     torch::Tensor output_tensor = torch::zeros({n, m}, options);
 
     // Switch between CPU and GPU
     if (input.is_cuda()) {
         /* GPU */
         // Create accessors
-        auto input_accessor = input.packed_accessor32<bool, 2, torch::RestrictPtrTraits>();
+        auto input_accessor = input.packed_accessor32<scalar, 2, torch::RestrictPtrTraits>();
         auto str_el_accessor = str_el.packed_accessor32<scalar, 2, torch::RestrictPtrTraits>();
+        auto footprint_accessor = footprint.packed_accessor32<bool, 2, torch::RestrictPtrTraits>();
         auto output_accessor = output_tensor.packed_accessor32<scalar, 2, torch::RestrictPtrTraits>();
 
         // Block & Grid parameters
@@ -212,23 +216,19 @@ torch::Tensor dilation(torch::Tensor input,
         const dim3 grid_size(grid_x, grid_y, 1);
 
         // Launch of the kernel
-        dilation_cuda_kernel<<<grid_size, block_size>>>(
-                input_accessor,
-                str_el_accessor,
-                output_accessor,
-                origin_x, origin_y, bottom);
+        dilation_cuda_kernel<<<grid_size, block_size>>>(input_accessor, str_el_accessor, footprint_accessor,
+                                                        output_accessor, origin_x, origin_y, bottom);
     } else {
         /* CPU */
         // Create accessors
-        auto input_accessor = input.accessor<bool, 2>();
+        auto input_accessor = input.accessor<scalar, 2>();
         auto str_el_accessor = str_el.accessor<scalar, 2>();
+        auto footprint_accessor = footprint.accessor<bool, 2>();
         auto output_accessor = output_tensor.accessor<scalar, 2>();
 
-        // Declare variables
         scalar value;
-        scalar current;
+        scalar sum;
         int idx_x, idx_y;
-
         // Computation
         for (int y = 0; y < n; y++) {
             for (int x = 0; x < m; x++) {
@@ -236,12 +236,12 @@ torch::Tensor dilation(torch::Tensor input,
                 // Compute the value of output[y][x]
                 for (int j = q-1; j >= 0; j--) {
                     for (int i = p-1; i >= 0; i--) {
-                        idx_x = x - (i - origin_x);
-                        idx_y = y - (j - origin_y);
-                        if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
-                            if (input_accessor[idx_y][idx_x]) {
-                                current = str_el_accessor[j][i];
-                                if (value < current) value = current;
+                        if (footprint_accessor[j][i]) {
+                            idx_x = x - (i - origin_x);
+                            idx_y = y - (j - origin_y);
+                            if (0 <= idx_x && idx_x < m && 0 <= idx_y && idx_y < n) {
+                                sum = input_accessor[idx_y][idx_x] + str_el_accessor[j][i];
+                                if (value < sum) value = sum;
                             }
                         }
                     }
